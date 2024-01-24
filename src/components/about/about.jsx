e4f9bf5dc051fe5d41e7315bab62df7f574c64b3
anapaulagarfias <paulagarfias4@gmail.com>
2024-01-24 14:52:36 -0600

    Iniciando info

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -5,24 +5,27 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import CardImage from "../../img/paula.jpg";
+import CardImage from "../../assets/paula.png";
 
 export default function MyCard() {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia
         component="img"
-        alt="green iguana"
-        height="140"
-        image="CardImage"
+        alt="Foto de Paula"
+        height="500"
+        image={CardImage}
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          Lizard
+          Ana Paula Garfias
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
+          Me considero una persona proactiva, con grandes interenses en seguirme
+          preparando para el futuro. A la par tengo una gran afición por las
+          tecnologías y su funcionalidad en el mundo actual por ello estoy
+          estudiando y preparandome para desenvolverme como ingeniera software
+          con amplios conocimientos en ciencia de datos.
         </Typography>
       </CardContent>
       <CardActions>
